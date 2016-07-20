535caaed6acb27c8ef600f229680ebbe0b0c42f5
Yuyi <liyuyi828@gmail.com>
2016-07-20 10:29:10 -0700

    complet index-links

--- lessons/09-index-links/modules/App.js
+++ lessons/09-index-links/modules/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import NavLink from './NavLink'
+// import { IndexLink } from 'react-router'
+// by using onlyActiveOnIndex, we don't need to import {IndexLink} property
 
 export default React.createClass({
   render() {
@@ -9,6 +11,7 @@ export default React.createClass({
         <ul role="nav">
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/repos">Repos</NavLink></li>
+          <li><NavLink to='/' onlyActiveOnIndex={true} >Home</NavLink></li>
         </ul>
         {this.props.children}
       </div>
