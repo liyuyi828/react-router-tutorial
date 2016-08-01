09638bdc1576fb3d1e01a60e07f27ff44c02a41a
Yuyi <liyuyi828@gmail.com>
2016-07-31 23:26:52 -0700

    finish server render

--- lessons/13-server-rendering/server.js
+++ lessons/13-server-rendering/server.js
@@ -1,3 +1,8 @@
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { match, RouteContext } from 'react-router'
+import routes from './module/routes'
+
 var express = require('express')
 var path = require('path')
 var compression = require('compression')
@@ -10,9 +15,32 @@ app.use(compression())
 app.use(express.static(path.join(__dirname, 'public')))
 
 // send all requests to index.html so browserHistory works
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+app.get('*', (req, res) => {
+  match({ routes: routes, location: req.url }, (err, redirect, props) => {
+    if (err) {
+      res.status(500).send(err.message)
+    } else if (redirect) {
+      res.redirect(redirect.pathname + redirect.search)
+    } else if (props) {
+      const appHtml = renderToString(<RouterContext {...props}/>)
+      res.send(renderPage(appHtml))
+    } else {
+      res.status(404).send('Not Found')
+    }
+  })
 })
+
+function renderPage(appHtml) {
+  return `
+    <!doctype html public="storage">
+    <html>
+    <meta charset=utf-8/>
+    <title>My First React Router App</title>
+    <link rel=stylesheet href=/index.css>
+    <div id=app>${appHtml}</div>
+    <script src="/bundle.js"></script>
+   `
+}
 
 var PORT = process.env.PORT || 8080
 app.listen(PORT, function() {
